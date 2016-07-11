74b5fdb9628da0413d9765cc9f1300458f962f15
Agustin Rios Reyes <nitsugario@gmail.com>
2016-07-11 18:01:17 -0500

    Add files via upload

---
+++ popup_dialogo.js
@@ -0,0 +1,121 @@
+function PopupDialogo( pstIdDiv )
+{
+	this.lstIdDiv     = pstIdDiv;
+	this.lnuConteTop  = 0;
+	this.lnuConteLeft = 0;
+	this.opciones     = {
+			lstColorFondo: '#fff',
+			lnuLeft: 0,
+			lnuTop:0,
+			lnuOpacity:75,
+			lnuZIndex: 100
+		}
+
+	var dialog = document.getElementById( this.lstIdDiv );
+
+	this.AnchoPagina = function()
+	{
+		return window.innerWidth != null ? window.innerWidth : document.documentElement && document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body != null ? document.body.clientWidth : null;
+	}
+
+	this.AltoPagina = function()
+	{
+		return window.innerHeight != null? window.innerHeight : document.documentElement && document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body != null? document.body.clientHeight : null;
+	}
+
+	this.PosicionTop = function()
+	{
+		return typeof window.pageYOffset != 'undefined' ? window.pageYOffset : document.documentElement && document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ? document.body.scrollTop : 0;
+	}
+
+	this.posicionLeft = function()
+	{
+		return typeof window.pageXOffset != 'undefined' ? window.pageXOffset : document.documentElement && document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft ? document.body.scrollLeft : 0;
+	}
+
+	this.MostrarDialogo = function()
+	{
+		var width       = this.AnchoPagina();
+		var height      = this.AltoPagina();
+		var left        = this.posicionLeft();
+		var top         = this.PosicionTop();
+
+		var dialogmask  = document.getElementById('dialog-mask');
+		if( dialogmask == null )
+		{
+			var dialogmask  = document.createElement('div');
+			dialogmask.id   = 'dialog-mask';
+			document.body.appendChild(dialogmask);
+
+			dialogmask.style.position   = 'absolute';
+			dialogmask.style.top        = this.opciones.lnuTop;
+			dialogmask.style.left       = this.opciones.lnuLeft;
+			dialogmask.style.width      = "100%";
+			dialogmask.style.height     = "100%";
+			dialogmask.style.backgroundColor = this.opciones.lstColorFondo;    
+			dialogmask.style.opacity    = '.'+this.opciones.lnuOpacity;
+			dialogmask.style.filter     = 'alpha(opacity='+this.opciones.lnuOpacity+')';
+			dialogmask.alpha            = this.opciones.lnuOpacity;
+			dialogmask.style.zIndex     = this.opciones.lnuZIndex;    
+		}
+
+		dialogmask.style.display    = ""; 
+
+		//var dialog = document.getElementById( this.lstIdDiv );
+		dialog.style.display    = "";        
+
+		var dialogwidth     = dialog.offsetWidth;
+		var dialogheight    = dialog.offsetHeight;
+
+		var topposition     = top + (height / 2) - (dialogheight / 2);
+		if( topposition < 0 )
+			topposition = 0;
+
+		var leftposition    = left + (width / 2) - (dialogwidth / 2);
+
+		dialog.style.top    = topposition + "px";
+		dialog.style.left   = leftposition + "px";
+		dialog.style.position   = 'absolute';
+		dialog.style.zIndex     = this.opciones.lnuZIndex+5;
+		dialog.style.display    = "block";
+	}
+
+	this.OcultarDialogo = function()
+	{
+		var dialogmask  = document.getElementById('dialog-mask');
+
+		if( dialogmask != null )
+			dialogmask.style.display    = "none"; 
+
+		//var dialog = document.getElementById( this.lstIdDiv );
+		dialog.style.display    = "none"; 
+	}
+	this.setBackgroundColor = function( pstColor )
+	{
+		var dialogmask  = document.getElementById('dialog-mask');
+
+		if( dialogmask != null )
+			dialogmask.style.backgroundColor = pstColor;
+	}
+	
+	this.setConteTop = function( pnuTop )
+	{
+		dialog.style.top = pnuTop;
+	}
+
+	this.setConteLeft = function( pnuLeft )
+	{
+		dialog.style.left = pnuLeft;
+	}
+	this.setOpacity = function( pnuOpcity )
+	{
+		var dialogmask  = document.getElementById('dialog-mask');
+
+		if( dialogmask != null )
+		{
+			dialogmask.style.opacity    = '.'+pnuOpcity;
+			dialogmask.style.filter     = 'alpha(opacity='+pnuOpcity+')';
+			dialogmask.alpha            = pnuOpcity;
+		}
+	}
+}
